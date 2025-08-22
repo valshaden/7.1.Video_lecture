8d97fc4bb54e55633a7ad22f88cd64f04d604750
valshaden <valshaden@yandex.ru>
2025-08-22 11:22:10 +0300

    + # 6.1.2. Создаем проект и репозиторий

--- 6.1.2. Creating a project and a repository.py
+++ 6.1.2. Creating a project and a repository.py
@@ -1,2 +1,30 @@
 # 6.1.1. API сайтов
 # 6.1.2. Создаем проект и репозиторий
+
+# Устанавливаем requests
+
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+# Создаем окно
+window = Tk()
+window.title("Котики")
+window.geometry("600x480")
+
+# Создаем метку без изображения
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    # Устанавливаем изображение в метку
+    label.config(image=img)
+    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
+    label.image = img  
+
+window.mainloop()
+
--- 6.1.3. Creating a function for uploading images.py
+++ 6.1.3. Creating a function for uploading images.py
@@ -1,3 +1,30 @@
 # 6.1.1. API сайтов
 # 6.1.2. Создаем проект и репозиторий
 # 6.1.3. Создаем функцию для загрузки изображений
+
+# Устанавливаем requests
+
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+# Создаем окно
+window = Tk()
+window.title("Котики")
+window.geometry("600x480")
+
+# Создаем метку без изображения
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    # Устанавливаем изображение в метку
+    label.config(image=img)
+    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
+    label.image = img  
+
+window.mainloop()
