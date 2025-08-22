5ca384725a59cc7672352d7b3f15380f9524bd4f
valshaden <valshaden@yandex.ru>
2025-08-22 12:05:48 +0300

    + # Создаём функцию

--- 6.1.2. Creating a project and a repository.py
+++ 6.1.2. Creating a project and a repository.py
@@ -8,6 +8,26 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+# Создаём функцию
+def load_image(url):
+    try:
+        # Отправляем GET-запрос с использованием requests.get()
+        response = requests.get(url)
+
+        # Проверяем успешность запроса (код ответа 200)
+        response.raise_for_status()
+
+        # Читаем байты из ответа в объект BytesIO
+        image_data = BytesIO(response.content)
+
+        # Открываем изображение с помощью PIL
+        img = Image.open(image_data)
+
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None
+
 # Создаем окно
 window = Tk()
 window.title("Котики")
